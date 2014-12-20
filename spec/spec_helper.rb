3992753843ff6c8d1e1310423b06b4858dd57627
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2014-12-20 22:21:03 +0100

    add some specs and travis ci integration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require "vimrunner"
+require "vimrunner/rspec"
+
+ROOT = File.expand_path('../..', __FILE__)
+
+Vimrunner::RSpec.configure do |config|
+  config.start_vim do
+    vim = Vimrunner.start
+
+    vim.add_plugin(File.join(ROOT, 'autoload'), 'spectacular.vim')
+
+    vim
+  end
+end
+
